d5a3cc9a2a88d4b15b311ac369576f56af970dbc
JPetsis <snowblind777@gmail.com>
2019-11-23 20:40:41 +0100

    add event listeners and display value

--- js/script.js
+++ js/script.js
@@ -5,3 +5,37 @@ const clear = document.getElementById("clear")
 const displayResult = document.getElementById("displayNumbers")
 const btnNumbers = document.getElementsByClassName("btnNumber")
 const btnOperators = document.getElementsByClassName("btnOperators")
+
+// Variables
+
+var displayVal = '0';
+var pendingVal;
+var evalStringArray = [];
+
+// Updating display
+
+updateDisplayVal = (e) => {
+  var btnText = e.target.innerText;
+  if(displayVal === "0") { 
+     displayVal = ""; 
+  }
+
+// Append the content of the button
+
+  displayVal += btnText;
+  displayResult.innerText = displayVal;
+}
+
+
+
+
+
+// Add event listeners to buttons
+
+for (let i = 0; i < btnNumbers.length; i++) {
+  btnNumbers[i].addEventListener('click', //function)
+}
+
+for (let i = 0; i < btnOperators.length; i++) {
+  btnOperators[i].addEventListener('click', //function);
+}
