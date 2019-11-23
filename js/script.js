9cf87c29f938a2f89435bfe729780c7ab01fb51c
JPetsis <snowblind777@gmail.com>
2019-11-23 19:56:58 +0100

    First commit and JS initialization

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Selectors
+
+const decimal = document.getElementById("decimal")
+const clear = document.getElementById("clear")
+const displayResult = document.getElementById("displayNumbers")
+const btnNumbers = document.getElementsByClassName("btnNumber")
+const btnOperators = document.getElementsByClassName("btnOperators")
